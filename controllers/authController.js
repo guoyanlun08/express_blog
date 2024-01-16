4b547e0f4bdf2b61475bdf08e92318b25501e0f6
v_yanlunguo <v_yanlunguo@tencent.com>
2024-01-16 18:45:58 +0800

    login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,60 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
 // 注册
 async function register(req, res) {
+  const { username, password, nickname } = req.body;
 
+  try {
+    const existingUser = await User.findOne({ where: { username } });
+    if (existingUser) {
+      res.status(400).json({ msg: "Username already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await User.create({ username, nickname, password: hashedPassword });
+
+    res.status(201).json({ msg: "User register successfully" });
+  } catch (e) {
+    res.status(500).json({ msg: "Failed to register user" });
+  }
 }
 
 // 登录
 async function login(req, res) {
+  const { username, password } = req.body;
 
+  try {
+    const user = await User.findOne({ where: { username } });
+    if (!user) {
+      res.status(401).json("Invalid username or password");
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      res.status(401).json("Invalid username or password");
+    }
+
+    user.lastOnlineTime = new Date();
+    await User.save();
+
+    const token = jwt.sign({ userId: user.id }, "xxx-your-secret-key", {
+      expiresIn: "24h",
+    });
+
+    res.json({
+      token,
+      account: user.username,
+      nickname: user.nickname,
+      userId: user.id,
+    });
+  } catch (e) {
+    res.status(500).json({ msg: "Fail to log in" });
+  }
 }
 
 module.exports = {
   register,
   login,
-}
+};
